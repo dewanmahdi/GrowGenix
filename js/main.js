8adec848265836497be590ffcdbf6564c2c8426a
Dewan Osman Mahdi <dewanmahdi@gmail.com>
2024-06-04 02:41:14 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function(){
+    // "user strick"
+    // $('.slider').ripples({
+    //     dropRadius: 10,
+    //     perturbance: 0.01,
+    // });
+
+    $(".text").typed({
+        strings: ["Produces IGF-1. <strong class='prosperity'>PROTEIN.</strong>",
+         "<strong class='prosperity'>Selected the molecule that is</strong> and <strong class='prosperity'>INSULIN GROWTH FACTOR</strong>"],
+        typespeed: 0,
+        loop: true
+    });
+});
+
+
+ 
